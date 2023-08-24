da9d347e67fc6294cf57ac85f68c5747363eaf11
abhavgoel <goelabhav2002@gmail.com>
2023-08-25 02:02:29 +0530

    msg route

---
+++ Graph/message_route.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define flash ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test int t;cin>>t;while(t--)
+#define loop(n) for(int i=0;i<n;i++)
+#define input(arr,n) for(int i=0;i<n;i++)cin>>arr[i];
+#define nline "\n"
+#define P pair<ll,pair<ll,ll>>
+using namespace std;
+using namespace chrono;
+int MOD=1000000007; 
+bool valid(int x,int y,int n,int m){return x>=0 && x<n && y>=0 && y<m;}
+int powMod(int a,int n){ ll ans=1;for(int i=1;i<=n;i++){ ans=(ans*a)%MOD;}return ans%MOD; }
+vector<int>dx={0,0,1,-1};
+vector<int>dy={1,-1,0,0};
+class DSU{
+  public:
+    vector<int>rank,parent,size;
+    
+    DSU(int n)
+    {
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+        parent[i]=i;
+    }
+    int findParent(int node)
+    {
+        if(node==parent[node])
+        return node;
+
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionByRank(int u,int v)
+    {
+        int ultParent_u = findParent(u);
+        int ultParent_v = findParent(v);
+
+        if(ultParent_u==ultParent_v)return;
+
+        else if(rank[ultParent_u]<rank[ultParent_v])
+        {
+            parent[ultParent_u]=ultParent_v;
+        }
+        else if(rank[ultParent_v]<rank[ultParent_u])
+        {
+            parent[ultParent_v]=ultParent_u;
+        }
+        else 
+        {
+            parent[ultParent_v]=ultParent_u;
+            rank[ultParent_u]++;
+        }
+
+    }
+    void unionBySize(int u,int v)
+    {
+        int ultParent_u = findParent(u);
+        int ultParent_v = findParent(v);
+
+        if(ultParent_u==ultParent_v)return;
+
+        else if(size[ultParent_u]<size[ultParent_v])
+        {
+            parent[ultParent_u]=ultParent_v;
+            size[ultParent_v]+=size[ultParent_u];
+        }
+        else if(size[ultParent_v]<size[ultParent_u])
+        {
+            parent[ultParent_v]=ultParent_u;
+            size[ultParent_u]+=size[ultParent_v];
+        }
+        else 
+        {
+            parent[ultParent_v]=ultParent_u;
+            size[ultParent_u]+=size[ultParent_v];
+        }
+    }
+};
+
+void solve()
+{
+  int n,m;
+  cin>>n>>m;
+  vector<vector<int>>graph(n+1);
+
+  for(int i=0;i<m;i++)
+  {
+    int u,v;
+    cin>>u>>v;
+
+    graph[u].pb(v);
+    graph[v].pb(u);
+
+  }
+  vector<int>vis(n+1);
+  vector<int>dist(n+1);
+  vector<int>prev(n+1);
+  queue<int>q;
+  q.push(1);
+  vis[1]=1;
+  while(!q.empty())
+  {
+    auto it = q.front();
+    q.pop();
+
+    for(auto i:graph[it])
+    {
+      if(vis[i]==0)
+      {
+        q.push(i);
+        vis[i]=1;
+        dist[i]=dist[it]+1;
+        prev[i]=it;
+      }
+    }
+
+  }
+
+  if(vis[n]==0)
+  {
+    cout<<"IMPOSSIBLE"<<nline;
+    return;
+  }
+
+  cout<<dist[n]+1<<nline;
+  int d = dist[n];
+  vector<int>path(d+1);
+  int endNode = n;
+  for(int i=d;i>=0;i--)
+  {
+    path[i] = endNode;
+    endNode = prev[endNode];
+
+
+  }
+  // reverse(path.begin(),path.end());
+
+  for(auto i:path)
+    cout<<i<<" "; 
+}
+ 
+int main()
+{
+  flash
+  
+  auto start1 = high_resolution_clock::now();
+    // test
+    solve();
+  auto stop1 = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop1 - start1);
+  cerr << "Time in miliseconds: " << duration.count() / 1000 << endl;
+  return 0;
+}
